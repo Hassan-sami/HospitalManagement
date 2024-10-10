a26b1817e01ac19f69fd405308836bebec63e37d
Hassan-sami <hassansami375@gmail.com>
2024-10-10 23:34:55 +0300

    10_11_PM_solveProgramConflict

--- Hospital.BLL/Mappers/DomainProfile.cs
+++ Hospital.BLL/Mappers/DomainProfile.cs
@@ -14,6 +14,18 @@ namespace Hospital.BLL.Mappers
         public DomainProfile() 
         {
             CreateMap<RegisterViewModel, Patient>();
+            CreateMap<Doctor, DoctorVm>();
+            CreateMap<Admin, AdminVm>();
+            CreateMap<Patient, PatientVm>();
+            CreateMap<MedicalRecord, MedicalRecordVm>();
+            CreateMap<Appointment, AppointmentVm>();
+
+
+            CreateMap<DoctorVm, Doctor>();
+            CreateMap<AdminVm, Admin>();
+            CreateMap<PatientVm, Patient>();
+            CreateMap<MedicalRecordVm, MedicalRecord>();
+            CreateMap<AppointmentVm, Appointment>();
         }
     }
 }
