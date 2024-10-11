cb60691bcc253cb31a48f7adafc9d7d51cb504b3
HassanElSayed21 <126038138+HassanElSayed21@users.noreply.github.com>
2024-10-11 18:07:21 +0300

    Delete wrong Icons

--- HospitalManagement/Controllers/HomeController.cs
+++ HospitalManagement/Controllers/HomeController.cs
@@ -15,15 +15,9 @@ namespace HospitalManagement.Controllers
 
         public IActionResult Index()
         {
-            // Example data
-            ViewBag.NumberOfDoctors = 5; // replace with actual data
-            ViewBag.NumberOfPatients = 20; // replace with actual data
-            ViewBag.NumberOfAppointments = 10; // replace with actual data
-            ViewBag.NumberOfMedicalRecords = 15; // replace with actual data
-            ViewBag.NewAppointments = 2; // replace with actual data
-            ViewBag.TodayPatients = 3; // replace with actual data
+       
 
-            return View(); // this will render the view with the layout
+            return View();
         }
         public IActionResult Privacy()
         {
