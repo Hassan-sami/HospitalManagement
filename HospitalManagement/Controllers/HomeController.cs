cec075598db5576fb5fe6a4e11956768476935f0
HassanElSayed21 <126038138+HassanElSayed21@users.noreply.github.com>
2024-10-11 18:03:42 +0300

    Dashboard , change Login , register and registered pages

--- HospitalManagement/Controllers/HomeController.cs
+++ HospitalManagement/Controllers/HomeController.cs
@@ -15,9 +15,16 @@ namespace HospitalManagement.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
+            // Example data
+            ViewBag.NumberOfDoctors = 5; // replace with actual data
+            ViewBag.NumberOfPatients = 20; // replace with actual data
+            ViewBag.NumberOfAppointments = 10; // replace with actual data
+            ViewBag.NumberOfMedicalRecords = 15; // replace with actual data
+            ViewBag.NewAppointments = 2; // replace with actual data
+            ViewBag.TodayPatients = 3; // replace with actual data
 
+            return View(); // this will render the view with the layout
+        }
         public IActionResult Privacy()
         {
             return View();
