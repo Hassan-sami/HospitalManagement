5b94bad3444eecd86e43de51d4fb19bcbc62721f
HassanElSayed21 <126038138+HassanElSayed21@users.noreply.github.com>
2024-10-15 22:15:09 +0300

    Add patient controller and some Services

---
+++ Hospital.BLL/Services/Abstraction/IAppointmentService.cs
@@ -0,0 +1,16 @@
+﻿using Hospital.BLL.ModelVM;
+using Hospital.DAL.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hospital.BLL.Services.Abstraction
+{
+    public interface IAppointmentService
+    {
+        Task<List<AppointmentVm>> GetAppointmentsByPatientId(string patientId);
+        Task<bool> CreateAppointment(Appointment appointment);
+        Task<AppointmentVm> GetAppointmentById(int id);
+        Task<bool> UpdateAppointment(AppointmentVm appointmentVm);
+        Task<bool> DeleteAppointment(int id);
+    }
+}
---
+++ Hospital.BLL/Services/Implementation/MedicalRecordService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Hospital.BLL.ModelVM;
+using Hospital.BLL.Services.Abstraction;
+using Hospital.DAL.Entities;
+using Hospital.DAL.Repository.Abstraction;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hospital.BLL.Services.Implementation
+{
+    public class MedicalRecordService : IMedicalRecordService
+    {
+        private readonly IMedicalRecordRepository _medicalRecordRepository;
+        private readonly IMapper _mapper;
+
+        public MedicalRecordService(IMedicalRecordRepository medicalRecordRepository, IMapper mapper)
+        {
+            _medicalRecordRepository = medicalRecordRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<MedicalRecordVm>> GetMedicalRecordsByPatientId(string patientId)
+        {
+            var medicalRecords = _medicalRecordRepository.GetAllMedicalRecords();
+            return _mapper.Map<List<MedicalRecordVm>>(medicalRecords.FindAll(m => m.PatientID == patientId));
+        }
+
+        public async Task<MedicalRecordVm> GetMedicalRecordById(int id)
+        {
+            var medicalRecord = _medicalRecordRepository.GetMedicalRecordById(id);
+            return _mapper.Map<MedicalRecordVm>(medicalRecord);
+        }
+
+        public async Task<bool> CreateMedicalRecord(MedicalRecord medicalRecord)
+        {
+            _medicalRecordRepository.AddMedicalRecord(medicalRecord);
+            return true;
+        }
+
+        public async Task<bool> UpdateMedicalRecord(MedicalRecordVm medicalRecordVm)
+        {
+            var medicalRecord = _mapper.Map<MedicalRecord>(medicalRecordVm);
+            _medicalRecordRepository.UpdateMedicalRecord(medicalRecord);
+            return true;
+        }
+
+        public async Task<bool> DeleteMedicalRecord(int id)
+        {
+            _medicalRecordRepository.DeleteMedicalRecord(id);
+            return true;
+        }
+    }
+}
